e8495497c95524838e629304fd20e97d823d4d7b
18485888 <AAlUstinov@sberbank.ru>
2020-11-13 01:20:42 +0300

    1.3. Вызов метода с аргументами. [#401897]

---
+++ src/main/java/ru/job4j/opp/Jukebox.java
@@ -0,0 +1,16 @@
+package ru.job4j.opp;
+
+public class Jukebox {
+    public void music(int position) {
+        if (position == 1) {
+            System.out.println("Спокойной ночи");
+        } else {
+            System.out.println("Песня не найдена");
+        }
+    }
+    public static void main(String[] args) {
+        Jukebox jukebox = new Jukebox();
+        jukebox.music(1);
+        jukebox.music(2);
+    }
+}
--- src/main/java/ru/job4j/opp/Student.java
+++ src/main/java/ru/job4j/opp/Student.java
@@ -3,15 +3,16 @@ package ru.job4j.opp;
 public class Student {
     public static void main(String[] args) {
         Student petya = new Student();
-        petya.music();
-        petya.music();
-        petya.music();
+        String song = "I believe, I can fly";
+        petya.music(song);
+        petya.music(song);
+        petya.music(song);
         petya.song();
         petya.song();
         petya.song();
     }
-    public void music() {
-        System.out.println("Tra tra tra");
+    public void music(String lyrics) {
+        System.out.println("I can sign a song : " + lyrics);
     }
     public void song(){
         System.out.println("I believe I can fly");
