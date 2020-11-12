3de07218f4903b6e8504baa36cc7faf28da329bc
18485888 <AAlUstinov@sberbank.ru>
2020-11-13 01:27:12 +0300

    1.4. Вызов метода с возвращаемым типом. [#401899]

---
+++ src/main/java/ru/job4j/opp/DummyDic.java
@@ -0,0 +1,13 @@
+package ru.job4j.opp;
+
+public class DummyDic {
+    public static void main(String[] args) {
+        DummyDic dummyDic = new DummyDic();
+        String searchTranslate = dummyDic.engToRus("tell");
+        System.out.println(searchTranslate);
+    }
+    public String engToRus(String eng) {
+        String translate = "Неизвестное слово. " + eng;
+        return translate;
+    }
+}
