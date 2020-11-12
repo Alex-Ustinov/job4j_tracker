683fcf0bb61569b29a5afbe87c24fe85da3ceaf0
18485888 <AAlUstinov@sberbank.ru>
2020-11-13 01:39:34 +0300

    1.5. Поля объекта [#401895]

--- src/main/java/ru/job4j/opp/Cat.java
+++ src/main/java/ru/job4j/opp/Cat.java
@@ -1,8 +1,25 @@
 package ru.job4j.opp;
 
 public class Cat {
+    private String food;
+    private String name;
+    public void show() {
+        System.out.println(this.name + " has eaten" + this.food);
+    }
+    public void eat(String meat){
+        this.food = meat;
+    }
+    public void giveNick(String nick) {
+        this.name = nick;
+    }
     public static void main(String[] args) {
-        Cat peppy = new Cat();
-        Cat sparky = new Cat();
+        Cat gav = new Cat();
+        gav.eat("kotleta");
+        gav.giveNick("gav");
+        gav.show();
+        Cat black = new Cat();
+        black.eat("fish");
+        black.giveNick("black cat");
+        black.show();
     }
 }
