70eb23793e2e6d783fde794949a1432722ea410d
18485888 <AAlUstinov@sberbank.ru>
2020-11-13 18:28:57 +0300

    1.7. Состояние объекта [#401900]

---
+++ src/main/java/ru/job4j/opp/Battery.java
@@ -0,0 +1,20 @@
+package ru.job4j.opp;
+
+public class Battery {
+    private int load;
+    Battery (int loadSize) {
+        this.load = loadSize;
+    }
+    public void exchange(Battery another) {
+        this.load = this.load + another.load;
+        another.load = 0;
+    }
+
+    public static void main(String[] args) {
+        Battery nokiaBattery = new Battery(100);
+        Battery samsungBattery = new Battery(400);
+        System.out.println("Nokia battery : " + nokiaBattery.load + ". Samsung battery : " + samsungBattery.load);
+        nokiaBattery.exchange(samsungBattery);
+        System.out.println("Nokia battery : " + nokiaBattery.load + ". Samsung battery : " + samsungBattery.load);
+    }
+}
