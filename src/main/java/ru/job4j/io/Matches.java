b206e306bdfceae21c34a029a27de70e6e9eecce
18485888 <AAlUstinov@sberbank.ru>
2020-11-19 01:04:15 +0300

    2. Scanner и чтение числа из консоли. [#401844]

---
+++ src/main/java/ru/job4j/io/Matches.java
@@ -0,0 +1,23 @@
+package ru.job4j.io;
+
+import java.util.Scanner;
+
+public class Matches {
+    public static void main(String[] args) {
+        int matches = 11;
+        System.out.println("Всего "+ matches + " спичек возьмите 1, 2 или 3  спички");
+        Scanner input = new Scanner(System.in);
+        while (matches > 3) {
+            int select = Integer.valueOf(input.nextLine());
+            if (select <= 3 && select > 0) {
+                matches = matches - select;
+                System.out.println("Осталось "+ matches + " спичек");
+            } else {
+                System.out.println("Вы можете взять не больше 3 спичек");
+            }
+        }
+        if (matches <= 3) {
+            System.out.println("Вы выйграли!");
+        }
+    }
+}
