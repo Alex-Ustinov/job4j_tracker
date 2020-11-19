1d8f3c401e8a49964c4e25ba8c791f8ccb4e9930
18485888 <AAlUstinov@sberbank.ru>
2020-11-20 01:36:47 +0300

    3. Интерфейс Input [#401845]

---
+++ src/main/java/ru/job4j/poly/Bus.java
@@ -0,0 +1,18 @@
+package ru.job4j.poly;
+
+public class Bus implements Transport {
+    @Override
+    public void drive() {
+
+    }
+
+    @Override
+    public void passengers(int amountPassengers) {
+
+    }
+
+    @Override
+    public double chargeOil(float amountOil) {
+        return 0;
+    }
+}
