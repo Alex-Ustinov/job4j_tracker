ff66888559f9716c90c23036c1b86e2ba3541286
18485888 <AAlUstinov@sberbank.ru>
2020-11-21 01:58:03 +0300

    4.1. Разрыв зависимости StartUI от Scanner. [#401849] refactoring

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -2,20 +2,22 @@ package ru.job4j.tracker;
 
 import ru.job4j.opp.ConsoleInput;
 
+import java.util.Scanner;
+
 public class StartUI {
 
     public void init(Input input, Tracker tracker) {
         boolean run = true;
         while (run) {
             this.showMenu();
-            int select = Integer.valueOf(input.askStr("Choose an option"));
+            int select = input.askInt("Choose an option");
             if (select == 1 ) {
                 Item [] items = tracker.findAll();
                 for (Item item : items) {
                         System.out.println(item);
                 }
             } else if (select == 2) {
-                int idItem = Integer.valueOf(input.askStr("Enter id:"));
+                int idItem = input.askInt("Enter id:");
                 String itemName = input.askStr("Enter item name:");
                 Item editItem = new Item();
                 editItem.setName(itemName);
@@ -26,7 +28,7 @@ public class StartUI {
                     System.out.println("Something went wrong");
                 }
             } else if (select == 3) {
-                int idItem = Integer.valueOf(input.askStr("Enter id:"));
+                int idItem = input.askInt("Enter id:");
                 boolean res = tracker.delete(idItem);
                 if (res) {
                     System.out.println("Item was deleted successfully");
@@ -34,7 +36,7 @@ public class StartUI {
                     System.out.println("Something went wrong");
                 }
             } else if (select == 4) {
-                int idItem = Integer.valueOf(input.askStr("Enter id:"));
+                int idItem = input.askInt("Enter id:");
                 Item foundItem = tracker.findById(idItem);
                 if (foundItem != null) {
                     System.out.println(foundItem);
@@ -68,11 +70,11 @@ public class StartUI {
     }
 
     public static void main(String[] args) {
-        Input consoleInput = new ConsoleInput();
+        Input input = new ConsoleInput();
         Tracker tracker = new Tracker();
         tracker.add(new Item(1, "Alex"));
         tracker.add(new Item(2, "Bill"));
         tracker.add(new Item(3, "Jon"));
-        new StartUI().init(consoleInput, tracker);
+        new StartUI().init(input, tracker);
     }
 }
