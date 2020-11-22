400462c0f1a56d1c0fd495d8254cdea6500ecbbe
18485888 <AAlUstinov@sberbank.ru>
2020-11-22 23:28:06 +0300

    5.1. Тестирование. Подготовка данных. [#401852]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -1,6 +1,8 @@
 package ru.job4j.tracker;
 
 import org.junit.Test;
+import ru.job4j.opp.ConsoleInput;
+
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
@@ -24,6 +26,30 @@ public class StartUITest {
         Tracker tracker = new Tracker();
         StartUI.createItem(input, tracker);
         assertThat("Crate item", is(input.askStr("test")));
+    }
+
+    @Test
+    public void whenReplaceItem() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("new item");
+        tracker.add(item);
+        String[] answers = {
+                ""+item.getId(),
+                "replaced item"
+        };
+        StartUI.replaceItem(new StubInput(answers), tracker);
+        Item replaced = tracker.findById(item.getId());
+        assertThat(replaced.getName(), is("replaced item"));
+    }
+
+    @Test
+    public void whenDeleteItem() {
+        Tracker tracker = new Tracker();
+        Item item = new Item(1,"delete item");
+        tracker.add(item);
+        StartUI.deteleItem(new ConsoleInput(), tracker);
+        String expected = null;
+        assertThat(tracker.findById(item.getId()), is(expected));
     }
 
 }
