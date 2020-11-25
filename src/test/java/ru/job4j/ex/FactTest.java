e89c4b727fe70b054fba38ac821d3a01b95caae2
18485888 <AAlUstinov@sberbank.ru>
2020-11-25 19:06:33 +0300

    0.3. Тестирование исключений с junit [#401864]

---
+++ src/test/java/ru/job4j/ex/FactTest.java
@@ -0,0 +1,11 @@
+package ru.job4j.ex;
+
+import org.junit.Test;
+
+public class FactTest {
+    @Test(expected = IllegalArgumentException.class)
+    public void whenNLessZeroCalc() {
+       Fact fact = new Fact();
+       fact.calc(-2);
+    }
+}
