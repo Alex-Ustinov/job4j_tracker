f14bc7ef8ef5b8990974637dd43a02adebd8dfbf
18485888 <AAlUstinov@sberbank.ru>
2020-12-22 00:31:13 +0300

    2. Отсортировать департаменты [#401812] refactoring

--- src/test/java/ru/job4j/collection/DepartmentsTest.java
+++ src/test/java/ru/job4j/collection/DepartmentsTest.java
@@ -1,7 +1,10 @@
 package ru.job4j.collection;
 
 import org.junit.Test;
+
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
@@ -21,5 +24,32 @@ public class DepartmentsTest {
         List<String> expect = Arrays.asList("k1", "k1/sk1");
         List<String> result = Departments.fillGaps(input);
         assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenSortDepartmentList() {
+        ArrayList<String> arrayList = new ArrayList<>(Arrays.asList(
+                "K2",
+
+                "K2/SK1",
+
+                "K2/SK1/SSK1",
+
+                "K2/SK1/SSK2",
+
+                "K1",
+
+                "K1/SK1",
+
+                "K1/SK1/SSK1",
+
+                "K1/SK1/SSK2",
+
+                "K1/SK2"));
+        List<String>dep = Departments.fillGaps(arrayList);
+        DepDescComp depDescComp = new DepDescComp();
+        Collections.sort(dep, depDescComp);
+
+        assertThat(arrayList, is(arrayList));
     }
 }
