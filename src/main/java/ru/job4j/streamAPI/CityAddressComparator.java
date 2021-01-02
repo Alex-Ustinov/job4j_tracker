d25584598fc72d3c43117ac2a828f5f48e8e365a
18485888 <AAlUstinov@sberbank.ru>
2021-01-02 21:43:40 +0300

    Уникальность элементов и сортировка. [#401657]

---
+++ src/main/java/ru/job4j/streamAPI/CityAddressComparator.java
@@ -0,0 +1,10 @@
+package ru.job4j.streamAPI;
+
+import java.util.Comparator;
+
+public class AddressComparator implements Comparator<Address> {
+    @Override
+    public int compare(Object o1, Object o2) {
+        return o1.
+    }
+}
